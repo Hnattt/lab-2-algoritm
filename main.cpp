89b6eb73b6773768e9c40fa63644f83fd33ebbfc
Hnattt <volodymyr.hnatchuk-ki241@nung.edu.ua>
2025-04-02 23:23:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,50 +3,69 @@
 #include <cmath>
 
 using namespace std;
-double calculateFunction(int x, int n) {
-    return pow(x, n) + sin(x);
+
+int y(int x, int n) {
+    if (x < 0) {
+        int result = 1;
+        for (int j = 2; j < n - 1; ++j) {
+            result *= (j + x);
+        }
+        return result;
+    } else {
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            int prod = 1;
+            for (int j = 0; j < n; ++j) {
+                prod *= (x + i + pow(j, 2));
+            }
+            result += prod;
+        }
+        return result;
+    }
 }
 int main() {
     ifstream inputFile("input.txt");
     ofstream outputFile("output.txt");
-    if (!inputFile || !outputFile) {
+    
+    if (!inputFile) {
         cerr << "Помилка відкриття файлу!" << endl;
         return 1;
     }
-    int rows, cols, n;
-    inputFile >> rows >> cols >> n;
-    int** array1 = new int*[rows];
-    double** array2 = new double*[rows];
     
-    for (int i = 0; i < rows; i++) {
-        array1[i] = new int[cols];
-        array2[i] = new double[cols];
-    }
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            inputFile >> array1[i][j];
+    int rows, cols;
+    inputFile >> rows >> cols;
+    
+    int** array = new int*[rows];
+    int** resultArray = new int*[rows];
+    
+    for (int i = 0; i < rows; ++i) {
+        array[i] = new int[cols];
+        resultArray[i] = new int[cols];
+        for (int j = 0; j < cols; ++j) {
+            inputFile >> array[i][j];
+            resultArray[i][j] = y(array[i][j], cols);
         }
     }
-    cout << "Результати обчислення:" << endl;
-    
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            array2[i][j] = calculateFunction(array1[i][j], n);
-            printf("%.2f ", array2[i][j]);
-            outputFile << array2[i][j] << " ";
-        }
-        cout << endl;
-        outputFile << endl;
-    }
-    for (int i = 0; i < rows; i++) {
-        delete[] array1[i];
-        delete[] array2[i];
-    }
-    delete[] array1;
-    delete[] array2;
     
     inputFile.close();
+    
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printf("%d ", resultArray[i][j]);
+            outputFile << resultArray[i][j] << " ";
+        }
+        printf("\n");
+        outputFile << endl;
+    }
+    
     outputFile.close();
+    
+    for (int i = 0; i < rows; ++i) {
+        delete[] array[i];
+        delete[] resultArray[i];
+    }
+    delete[] array;
+    delete[] resultArray;
     
     return 0;
 }
